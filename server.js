ee56dcbd376b12c25e612134d189054fe2631db6
kristajg <krista.goralczyk@gmail.com>
2022-01-22 10:29:04 -0600

    add express server

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+// 3rd party libraries
+import path from 'path';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Express server listening on port: ${port}`);
+});
