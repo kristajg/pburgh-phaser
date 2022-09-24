eb4394813d7b5357852b5630e21a0a54fc7afb18
kristajg <krista.goralczyk@gmail.com>
2022-09-24 15:19:43 -0600

    Move all text box visibility logic into UIScene

--- src/scenes/UIScene.js
+++ src/scenes/UIScene.js
@@ -3,6 +3,7 @@ import eventsCenter from '../prefabs/EventsCenter';
 import { createTextBox, createTextContentList, createSelectionOptions } from '../utils/textBox';
 
 let textBox;
+let textBoxOpen = false;
 let currentTextIndex = 0;
 let textContentList = [];
 let textSelectionOptions = [];
@@ -13,10 +14,8 @@ export default class UIScene extends Phaser.Scene {
   }
 
   create() {
-    // text box listeners
-    eventsCenter.on('show-text-box', this.showTextBox, this);
-    eventsCenter.on('hide-text-box', this.hideTextBox, this);
-    eventsCenter.on('advance-text-box', this.advanceTextBox, this);
+    // event listeners
+    eventsCenter.on('enter-key-pressed', this.enterKeyPressed, this);
 
     // this.label = this.add.text(10, 10, 'UISCENE', { fontSize: 28 });
     // listen to 'update-count' event & call `updateCount()` when it fires
@@ -25,15 +24,13 @@ export default class UIScene extends Phaser.Scene {
     // clean up when Scene is shutdown
     this.events.on(Phaser.Scenes.Events.SHUTDOWN, () => {
       // eventsCenter.off('update-count', this.updateCount, this);
-      eventsCenter.off('show-text-box', this.showTextBox, this);
-      eventsCenter.off('hide-text-box', this.hideTextBox, this);
-      eventsCenter.off('advance-text-box', this.advanceTextBox, this);
+      eventsCenter.off('enter-key-pressed', this.enterKeyPressed, this);
     });
   }
 
-  showTextBox(data) {
-    const { scene, currentInteractiveName } = data;
+  showTextBox(scene, currentInteractiveName) {
     if(!textBox){
+      textBoxOpen = true;
       textContentList = createTextContentList(currentInteractiveName);
       textBox = createTextBox(scene, textContentList[0]);
     }
@@ -42,37 +39,36 @@ export default class UIScene extends Phaser.Scene {
   hideTextBox(player) {
     if(textBox) {
       const { textBoxBackGround, textBoxContent } = textBox;
+      textBoxOpen = false;
+      currentTextIndex = 0;
       textBoxBackGround.destroy();
       textBoxContent.destroy();
       textBox = null;
+      console.log('...player? ', player);
       player.body.moves = true;
     }
   }
 
-  advanceTextBox(data) {
-    const { scene, player } = data;
-
-    if(currentTextIndex !== 0){
+  advanceTextBox(scene, player) {
+    currentTextIndex++;
+  
+    if (currentTextIndex >= textContentList.length) {
+      // close textbox if the end of content is reached
+      this.hideTextBox(player);
+    } else {
+      // proceed to next contect
       const { textBoxContent } = textBox;
       const isSelectionObject = typeof textContentList[currentTextIndex] === 'object';
       if (isSelectionObject) {
         // Set main text as the selection prompt
         textBoxContent.setText(textContentList[currentTextIndex].mainText);
-
+  
         // add selections to the textbox
         textSelectionOptions = createSelectionOptions(scene, textContentList[currentTextIndex].options);
       } else {
         // advance textContent to the next item in the array
         textBoxContent.setText(textContentList[currentTextIndex]);
       }
-    }
-    currentTextIndex++;
-
-    // close textbox if the end of content is reached
-    if(currentTextIndex > textContentList.length) {
-      eventsCenter.emit('hide-text-box', player);
-      eventsCenter.emit('toggle-text-box-visibility', false);
-      currentTextIndex = 0;
     }
   }
 
@@ -83,10 +79,20 @@ export default class UIScene extends Phaser.Scene {
   //   currentTextIndex = 0;
   // }
 
-  selectDialogOption(options) {
-    // Handles up, down, enter
-    console.log('select dialog option fired ', options);
-    // update localStorage with selection
+  // selectDialogOption(options) {
+  //   // Handles up, down, enter
+  //   console.log('select dialog option fired ', options);
+  //   // update localStorage with selection
+  // }
+
+  enterKeyPressed(data) {
+    const { scene, player, currentInteractiveName } = data;
+    if (!textBoxOpen) {
+      this.showTextBox(scene, currentInteractiveName);
+    } else {
+      this.advanceTextBox(scene, player);
+    }
+    // TODO: handle other enter key presses here (select option)
   }
 
   // updateCount(count) {
